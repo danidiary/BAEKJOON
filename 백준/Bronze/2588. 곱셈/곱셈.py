35f813584dbe4b86aa81a2ac884be6da1d9b4acd
haewon <166496923+danidiary@users.noreply.github.com>
2024-04-16 18:37:04 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,7 @@
+h = int(input())      # int is for when you might use not only words but numbers. :D
+w = input()
+
+print(h*int(w[2]))  # write the index with the brackets and use int when you multiply by another int.
+print(h*int(w[1]))
+print(h*int(w[0]))
+print(h*int(w))
