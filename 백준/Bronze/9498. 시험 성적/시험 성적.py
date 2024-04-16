7700a0d4724e20cb6ff336a389f080d7041e24a2
haewon <166496923+danidiary@users.noreply.github.com>
2024-04-16 19:25:04 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+if(90<=score<=100):     # put score in the middle so it means in between!!!!!
+  print("A")
+elif(80<=score<=89):
+  print("B")
+elif(70<=score<=79):
+  print("C")
+elif(60<=score<=69):
+  print("D")
+else:
+  print("F")
