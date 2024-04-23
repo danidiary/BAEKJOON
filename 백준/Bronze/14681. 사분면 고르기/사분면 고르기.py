7313c367713077bb1d3b5be66f2e8dc42cdb9c2e
haewon <166496923+danidiary@users.noreply.github.com>
2024-04-23 19:22:40 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,10 @@
+X = int(input())
+Y = int(input())
+if X > 0 and Y > 0:   # write and in the middle of the two
+  print(1)
+elif X < 0 and Y > 0:
+  print(2)
+elif X < 0 and Y < 0:
+  print(3)
+elif X > 0 and Y < 0:
+  print(4)
